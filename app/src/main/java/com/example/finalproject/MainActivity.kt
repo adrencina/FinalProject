ad8dfe7f12a65bbc3a6166f5edf0452719e4615e
ricar <ricardosr4@gmail.com>
2024-06-05 21:49:43 -0400

    update main activity

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,24 +2,27 @@ package com.example.finalproject
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        navigateToPreLogin()
+
+      navigateToPreLogin()
+
+
     }
-    private fun navigateToPreLogin() {
+    fun navigateToPreLogin(){
         val intent = Intent(this, PreLoginActivity::class.java)
         startActivity(intent)
         finish()
     }
+
+
+
 }
