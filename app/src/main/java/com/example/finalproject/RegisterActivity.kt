0e7a7d01f8f0ae7d3dd230d827f4979acd5afcd8
Adri Encina <adr.encina@gmail.com>
2024-06-07 04:05:29 -0300

    update Registeractivity.

--- app/src/main/java/com/example/finalproject/RegisterActivity.kt
+++ app/src/main/java/com/example/finalproject/RegisterActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.finalproject.databinding.ActivityLoginBinding
+import com.example.finalproject.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
