c7bbc8864616daec289c65e16d47796084f27c5f
Adri Encina <adr.encina@gmail.com>
2024-06-10 22:16:40 -0300

    1er commit. pull de develop para seguir codigo.

--- app/src/main/java/com/example/finalproject/RegisterActivity.kt
+++ app/src/main/java/com/example/finalproject/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.util.PatternsCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.finalproject.databinding.ActivityRegisterBinding
@@ -19,5 +22,53 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        validateEmail()
+        validateInputs()
     }
+
+    private fun isValidEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun validateInputs(): Boolean {
+        val email = binding.etEmail.text.toString()
+        var isValid = true
+
+        if (!isValidEmail(email)) {
+            binding.tvErrorRegister.visibility = View.VISIBLE
+            isValid = false
+        } else {
+            binding.tvErrorRegister.visibility = View.GONE
+        }
+
+        // Additional validation for other fields can go here
+
+        return isValid
+    }
+
+
+
+
+
+
+
+
+
+
+//    private fun validateEmail(): Boolean {
+//        val email = binding.etEmail.text.toString()
+//        return if (email.isEmpty()) {
+//            binding.etEmail.error = "Ingrese su Email"
+//            false
+//        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+//            binding.etEmail.error = "Email inválido"
+//            false
+//        } else {
+//            binding.etEmail.error = null
+//            true
+//        }
+//    }
+
+
+
 }
