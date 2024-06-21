f4591d47bacdf13425877ece51efa0a0d1dcfb71
ricar <ricardosr4@gmail.com>
2024-06-21 10:42:13 -0400

    rla-19 ok

--- app/src/main/java/com/example/finalproject/ui/home/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/finalproject/ui/home/viewModel/HomeViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.finalproject.ui.home.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.finalproject.data.service.dto.HomeState
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
+
+    private val _homeState = MutableLiveData<HomeState>()
+    val homeState: LiveData<HomeState> = _homeState
 }
