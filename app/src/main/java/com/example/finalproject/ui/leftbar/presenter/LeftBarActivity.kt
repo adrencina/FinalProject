ce86a9b1c0dfab8a67fb5860ad045b24dea6942f
Adri Encina <adr.encina@gmail.com>
2024-07-08 02:31:40 -0300

    Update Leftbar...

--- app/src/main/java/com/example/finalproject/ui/leftbar/presenter/LeftBarActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/leftbar/presenter/LeftBarActivity.kt
@@ -1,19 +1,18 @@
 package com.example.finalproject.ui.leftbar.presenter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.finalproject.R
 import com.example.finalproject.databinding.ActivityLeftBarBinding
-import com.google.android.material.navigation.NavigationView
+import com.example.finalproject.ui.home.presenter.HomeActivity
 
 class LeftBarActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLeftBarBinding
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,39 +20,50 @@ class LeftBarActivity : AppCompatActivity() {
         binding = ActivityLeftBarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        drawerLayout = binding.root
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
-        val navigationView = binding.navigationView
-        NavigationUI.setupWithNavController(navigationView, navController)
+        setupActionBarWithNavController(navController)
 
-        setupDrawerContent(navigationView)
+        val initialFragment = intent.getIntExtra("initialFragment", R.id.imagesFragment)
+        navController.navigate(initialFragment)
+
+        setupButtonNavigation()
+        setupBackIcon()
     }
 
-    private fun setupDrawerContent(navigationView: NavigationView) {
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawerLayout.closeDrawers()
-            when (menuItem.itemId) {
-                R.id.nav_commentsFragment -> {
-                    navController.navigate(R.id.commentsFragment)
-                }
-
-                R.id.nav_financingFragment -> {
-                    navController.navigate(R.id.financingFragment)
-                }
-
-                R.id.nav_descriptionFragment -> {
-                    navController.navigate(R.id.descriptionFragment)
-                }
-
-                R.id.nav_imagesFragment -> {
-                    navController.navigate(R.id.imagesFragment)
-                }
-            }
-            true
+    private fun setupButtonNavigation() {
+        binding.navImagesFragment.setOnClickListener {
+            navController.navigate(R.id.imagesFragment)
         }
+        binding.navDescriptionFragment.setOnClickListener {
+            navController.navigate(R.id.descriptionFragment)
+        }
+        binding.navFinancingFragment.setOnClickListener {
+            navController.navigate(R.id.financingFragment)
+        }
+        binding.navCommentsFragment.setOnClickListener {
+            navController.navigate(R.id.commentsFragment)
+        }
+    }
+
+    private fun setupBackIcon() {
+        binding.backIcon.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
