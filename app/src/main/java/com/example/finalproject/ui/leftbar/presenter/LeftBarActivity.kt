c4c5a5fc43f57d344d38859288278499b54bf1c1
ricar <ricardosr4@gmail.com>
2024-07-20 19:44:04 -0400

    primer ajuste ok

--- app/src/main/java/com/example/finalproject/ui/leftbar/presenter/LeftBarActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/leftbar/presenter/LeftBarActivity.kt
@@ -21,57 +21,57 @@ class LeftBarActivity : AppCompatActivity() {
         binding = ActivityLeftBarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_container) as NavHostFragment
-        navController = navHostFragment.navController
-
-        setupActionBarWithNavController(navController)
-
-        val initialFragment = intent.getIntExtra("initialFragment", R.id.imagesFragment)
-        navController.navigate(initialFragment)
-
-        setupButtonNavigation()
-        setupBackIcon()
-
-        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                val intent = Intent(this@LeftBarActivity, HomeActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        })
-    }
-
-    private fun setupButtonNavigation() {
-        binding.navImagesFragment.setOnClickListener {
-            navigateToFragment(R.id.imagesFragment)
-        }
-        binding.navDescriptionFragment.setOnClickListener {
-            navigateToFragment(R.id.descriptionFragment)
-        }
-        binding.navFinancingFragment.setOnClickListener {
-            navigateToFragment(R.id.financingFragment)
-        }
-        binding.navCommentsFragment.setOnClickListener {
-            navigateToFragment(R.id.commentsFragment)
-        }
-    }
-
-    private fun navigateToFragment(fragmentId: Int) {
-        navController.navigate(fragmentId)
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-    }
-
-    private fun setupBackIcon() {
-        binding.backIcon.setOnClickListener {
-            onSupportNavigateUp()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
-        finish()
-        return true
-    }
+//        val navHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.fragment_container) as NavHostFragment
+//        navController = navHostFragment.navController
+//
+//        setupActionBarWithNavController(navController)
+//
+//        val initialFragment = intent.getIntExtra("initialFragment", R.id.imagesFragment)
+//        navController.navigate(initialFragment)
+//
+//        setupButtonNavigation()
+//        setupBackIcon()
+//
+//        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+//            override fun handleOnBackPressed() {
+//                val intent = Intent(this@LeftBarActivity, HomeActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            }
+//        })
+//    }
+//
+//    private fun setupButtonNavigation() {
+//        binding.navImagesFragment.setOnClickListener {
+//            navigateToFragment(R.id.imagesFragment)
+//        }
+//        binding.navDescriptionFragment.setOnClickListener {
+//            navigateToFragment(R.id.descriptionFragment)
+//        }
+//        binding.navFinancingFragment.setOnClickListener {
+//            navigateToFragment(R.id.financingFragment)
+//        }
+//        binding.navCommentsFragment.setOnClickListener {
+//            navigateToFragment(R.id.commentsFragment)
+//        }
+//    }
+//
+//    private fun navigateToFragment(fragmentId: Int) {
+//        navController.navigate(fragmentId)
+//        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+//    }
+//
+//    private fun setupBackIcon() {
+//        binding.backIcon.setOnClickListener {
+//            onSupportNavigateUp()
+//        }
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val intent = Intent(this, HomeActivity::class.java)
+//        startActivity(intent)
+//        finish()
+//        return true
+   }
 }
