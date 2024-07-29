1351e295eadcc61a23f6bdead61327ebce0dfe7a
MavRial <maverickgshepherd@gmail.com>
2024-07-29 17:44:17 -0300

    update

--- app/src/main/java/com/example/finalproject/ui/leftbar/fragments/description/state/DescriptionState.kt
+++ app/src/main/java/com/example/finalproject/ui/leftbar/fragments/description/state/DescriptionState.kt
@@ -4,7 +4,6 @@ package com.example.finalproject.ui.leftbar.fragments.description.state
 import com.example.finalproject.data.dto.response.Product
 
 
-
 sealed class DescriptionState {
     data object Loading :  DescriptionState()
     data class Success(val product: Product) : DescriptionState()
